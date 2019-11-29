921745aa82caa64f0240d0c9051b800fb88e7bc3
komeil <komeilr@gmail.com>
2019-11-29 12:59:59 -0500

    message

--- app/factory.py
+++ app/factory.py
@@ -4,21 +4,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-
+# instantiate db
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    
+    # Set app configuration
     if os.environ.get('FLASK_ENV') == 'production':
         app.config.from_object(config.ProductionConfig)
     else:
         app.config.from_object(config.DevelopmentConfig)
-        
-    db.init_app(app)
 
+    # initialize DB
+    db.init_app(app)
+ 
     with app.app_context():
+        #TODO: Update and populate db
+
+        # Add Routes
         from app.main.routes import main_bp
         app.register_blueprint(main_bp)
 
