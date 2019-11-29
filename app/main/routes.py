9a5d590f2e2f7404a9c0a5d18d0311b3b34a2556
komeil <komeilr@gmail.com>
2019-11-29 11:00:38 -0500

    message

---
+++ app/main/routes.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__, url_prefix='/', template_folder='templates')
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html', title="Index")
+    
