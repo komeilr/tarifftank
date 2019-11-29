6f24219126e31d656b3c1f735c0187933a9a9bd4
komeil <komeilr@gmail.com>
2019-11-29 12:03:18 -0500

    message

--- app/factory.py
+++ app/factory.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(configObject):
     app = Flask(__name__)
     app.config.from_object(configObject)
+
+    db.init_app(app)
 
     with app.app_context():
         from app.main.routes import main_bp
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from app.factory import create_app
+from app.factory import create_app, db
 import config
 
 app = create_app(config.DevelopmentConfig)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
