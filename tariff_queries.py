ecbf0f534ef94644eed9b77b3ba0ed179578772b
komeil <komeilr@gmail.com>
2019-12-03 16:52:47 -0500

    message

--- tariff_queries.py
+++ tariff_queries.py
@@ -1,27 +1,50 @@
 import app.ca.models
+from sqlalchemy import and_
 
 class TariffRateCA:
     def __init__(self, tariff, year='2019'):
         self.tariff = tariff
         self.year = year
         self.tobjs = self.get_rate_info()
+        self.uom = self.tobjs[-1].uom
         self.ftas =  [
             'mfn', 'gt', 'aut', 'nzt', 'ccct', 'ldct', 
             'gpt', 'ust', 'mt', 'must', 'ciat', 'ct', 
             'crt', 'it', 'nt', 'slt', 'pt', 'colt', 
             'jt', 'pat', 'hnt', 'krt', 'ceut', 'uat', 'cptpt'
             ]
+        self.process()
 
     def get_rate_info(self):
         queryClass = vars(app.ca.models)[f"CA{self.year}"]
     
         results = []
         for i in range(4, 11):
-            q = queryClass.query.filter_by(tariff=self.tariff[:i]).first()
+            q = queryClass.query.filter(
+                and_(
+                    queryClass.tariff.like(self.tariff[:i]), 
+                    queryClass.description.isnot(None)
+                    )).first()
             if q:
                 results.append(q)
 
         return results
+
+    def process(self):
+        """performs basic processing on tariff rates for easier access to data"""
+
+        self.descriptions = {}
+        self.rates = {}
+
+        for tariff in self.tobjs:
+            if tariff.description:
+                self.descriptions[tariff.tariff] = tariff.description
+
+            if tariff.mfn:
+                for fta in self.ftas:
+                    if hasattr(tariff, fta) and tariff.__getattribute__(fta):
+                        self.rates[fta] = tariff.__getattribute__(fta)
+
 
     def __str__(self):
         out_description = ""
@@ -33,9 +56,13 @@ class TariffRateCA:
                 out_description += f"{tariff.tariff} {tariff.description}"
                 out_description += "\n"
 
-            if tariff.uom:
+            if tariff.mfn:
                 for fta in self.ftas:
-                    out_rates += f"{fta}\t{tariff.__getattribute__(fta)}\t{tariff.uom}\n"
+                    if hasattr(tariff, fta) and tariff.__getattribute__(fta):
+                        rate = tariff.__getattribute__(fta)
+                        if rate == 'Free':
+                            rate = ''
+                        out_rates += f"{fta}\t{rate}\t{self.uom}\n"
 
         return out_description + out_rates
                 
