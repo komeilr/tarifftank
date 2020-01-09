5c54a40741d22c3f84dec9fd1c67c7bea7b0e22b
komeil <komeilr@gmail.com>
2020-01-09 17:22:28 -0500

    message

--- config.py
+++ config.py
@@ -17,11 +17,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_POOL_SIZE = 20
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_size':20,
-        'pool_recycle':120,
-        'pool_pre_ping':True
-    }
+
 
     RATELIMIT_DEFAULT = "1/second"
 
@@ -30,6 +26,11 @@ class DevelopmentConfig(Config):
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     DEBUG_TB_PROFILER_ENABLED = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_size':20,
+    'pool_recycle':120,
+    'pool_pre_ping':True
+}
     #SQLALCHEMY_ECHO = True
 
 
@@ -38,6 +39,8 @@ class ProductionConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_POOL_TIMEOUT = 20
+    SQLALCHEMY_POOL_SIZE= 20
+    SQLALCHEMY_POOL_RECYCLE= 120
     
 
 class TestingConfig(Config):
