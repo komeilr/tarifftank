051cad99964c5d7b0fbf874e35db343aa68ce52a
komeil <komeilr@gmail.com>
2020-01-13 15:57:01 -0500

    message

--- app/static/js/quicksearch.js
+++ app/static/js/quicksearch.js
@@ -1,5 +1,5 @@
 if (window.location.href.length > 26) {
-    document.getElementById("navSearchBar").style.visibility = "hidden";
-} else {
     document.getElementById("navSearchBar").style.visibility = "visible";
+} else {
+    document.getElementById("navSearchBar").style.visibility = "hidden";
 };
